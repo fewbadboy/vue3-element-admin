767bf496c98c4ad4803237a533554bcd764f0bb7
fewbadboy <renekton.king@qq.com>
2022-04-11 18:29:42 +0800

    base

---
+++ src/settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: 'Vue3 Element Admin',
+  /**
+   * @type {boolean} true | false
+   * @dessciption Whether show zhe settings right-panel
+   */
+  showSettings: true
+}
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,19 @@
+const path = require('path')
+const { title } = require('process')
+const { title } = require('./src/settings')
+
+function resolve(dir) {
+  return path.resolve(__dirname, dir)
+}
+
+const name = title || 'Vue3 Element Admin'
+
 module.exports = {
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: config => {
+
+  },
+  chainWebpack: config => {
+    
+  }
 }
