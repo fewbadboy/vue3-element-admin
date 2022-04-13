fef0027a2e7fce42f13d4591d3aa06be57e6a575
fewbadboy <renekton.king@qq.com>
2022-04-13 18:32:03 +0800

    base

--- vue.config.js
+++ vue.config.js
@@ -5,10 +5,31 @@ function resolve(dir) {
   return path.resolve(__dirname, dir)
 }
 
+const port = process.env.port || 8080
+
 const name = title || 'Vue3 Element Admin'
 
 module.exports = {
-  transpileDependencies: true,
+  /**
+   * If you plan to deploy your site to https://foo.github.io/bar/
+   * then publicPath should be set to "/bar/"
+   * Detail: https://cli.vuejs.org/config/#publicpath
+   */
+  publicPath: '/',
+  outputDir: 'web',
+  assetsDir: 'static',
+  lintOnSave: process.env.NODE_ENV === 'development',
+  productionSourceMap: false,
+  // https://github.com/vuejs/vue-cli 4.5.4 used webpack 5
+  devServer: {
+    port,
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false
+      }
+    }
+  },
   // https://github.com/survivejs/webpack-merge
   configureWebpack: {
     name,
