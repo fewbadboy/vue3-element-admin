315065002e091918ec063bbdff72908e2022e648
fewbadboy <1294234810@qq.com>
2022-04-21 00:43:49 +0800

    login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="login-container">
     <el-form
+      ref="loginForm"
       :model="loginForm"
       :rules="loginRules"
       label-position="left"
+      class="login-form"
     >
       <el-form-item prop="username">
-        <el-input v-model="loginForm.username" />
+        <el-input v-model="loginForm.username" :prefix-icon="Search" />
       </el-form-item>
       <el-form-item prop="password">
-        <el-input v-model="loginForm.password" type="password" autocomplete="off" />
+        <el-input v-model="loginForm.password" type="password" show-password autocomplete="off" :prefix-icon="Search" />
       </el-form-item>
-      <el-form-item>
+      <el-form-item style="text-align: center">
         <el-button type="primary">
           Submit
         </el-button>
@@ -43,5 +45,11 @@ $login_bg: #2d3a4b;
 .login-container {
   height: 100%;
   background-color: $login_bg;
+
+  .login-form {
+    width: 520px;
+    margin: 0 auto;
+    padding: 160px 35px 0;
+  }
 }
 </style>
