6e9ea0462ad35c5dace8c02b189abbae18ceb1f9
fewbadboy <renekton.king@qq.com>
2022-04-25 16:00:04 +0800

    base sidebar

---
+++ src/layout/components/Sidebar/LogoPart.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="sidebar-logo-container" :class="{'is-collapse': collapse}">
+    <router-link class="sidebar-logo-link" to="/">
+      <img :src="logo" class="sidebar-logo">
+      <transition name="sidebar-logo">
+        <div v-if="collapse" class="sidebar-title">
+          {{ title }}
+        </div>
+      </transition>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    collapse: {
+      type: Boolean,
+      required: true,
+      default: true
+    }
+  },
+  data() {
+    return {
+      title: 'vue3-element-admin',
+      logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.sidebar-logo-enter-active {
+  transition: opacity 1.5s;
+}
+.sidebar-logo-enter-from,
+.sidebar-logo-leave-to {
+  opacity: 0;
+}
+
+.sidebar-logo-container {
+  height: 50px;
+  line-height: 50px;
+  background: #2b2f3a;
+
+  & .sidebar-logo-link {
+    width: 100%;
+    height: 100%;
+
+    & .sidebar-logo {
+      width: 32px;
+      height: 32px;
+      vertical-align: middle;
+    }
+
+    & .sidebar-title {
+      display: inline-block;
+      color: #fff;
+      font-weight: 600;
+      vertical-align: middle;
+    }
+  }
+}
+</style>
--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,20 +1,66 @@
 <template>
   <div :class="{'has-logo': showLogo}">
-    <span>Logo</span>
-    <el-scrollbar>
-      <div>
-        Sidebar
-      </div>
+    <logo-part :collapse="isCollapse" />
+    <el-scrollbar wrap-class="scrollbar-wrapper">
+      <el-menu
+        mode="vertical"
+        :default-active="activeMenu"
+        :collapse="isCollapse"
+        :background-color="variables.menuBgColor"
+        :text-color="variables.menuText"
+        :active-text-color="variables.menuActiveText"
+        :unique-opened="false"
+      >
+        <el-sub-menu index="1">
+          <template #title>
+            <svg-icon icon-class="dashboard" />
+            <span>Navigator One</span>
+          </template>
+          <el-sub-menu index="1-4">
+            <template #title>
+              item four
+            </template>
+            <el-menu-item index="1-4-1">
+              item one
+            </el-menu-item>
+            <el-menu-item index="1-4-2">
+              item two
+            </el-menu-item>
+          </el-sub-menu>
+        </el-sub-menu>
+        <el-menu-item index="2">
+          <svg-icon icon-class="dashboard" />
+          <span>Navigator Two</span>
+        </el-menu-item>
+      </el-menu>
     </el-scrollbar>
   </div>
 </template>
 
 <script>
+import LogoPart from './LogoPart'
+import variables from '@/styles/variables.scss'
+console.log(variables)
 export default {
+  components: {
+    LogoPart
+  },
   data() {
     return {
-      showLogo: true
+      showLogo: true,
+      activeMenu: '2'
     }
+  },
+  computed: {
+    isCollapse() {
+      return false
+    },
+    variables() {
+      return variables
+    }
+  },
+  mounted() {
+    console.log(variables)
   }
 }
 </script>
